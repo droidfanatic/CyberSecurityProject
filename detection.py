11e35e319b533f0726ae4337184c4ac2507d19b8
Daycee Duncan <42853019+droidfanatic@users.noreply.github.com>
2022-11-22 17:21:53 -0600

    Detection Script

---
+++ detection.py
@@ -0,0 +1,28 @@
+import os
+
+pids = []
+
+for pid in os.listdir('/proc'):
+	if pid.isdigit():
+		pids.append(pid)
+
+f = open("syspids.txt", "w")
+
+for pid in pids:
+	f.write(str(pid) + "\n")
+
+f.close()
+
+alertfile = open("alertfile.txt", "w")
+
+while(True):
+	#read monitoring file
+	f = open("syspids.txt", "r")
+	lines = f.readlines()
+	f.close()
+
+	for line in lines:
+		#look for weirdness
+		if line == "weirdness":
+			print("Alert! Attack in progress!")
+			alertfile.write("Alert! Attack in progress!")
