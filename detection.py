d63ea2718d4dcaae9bca1569d26c684c96b54c3e
Daycee Duncan <42853019+droidfanatic@users.noreply.github.com>
2022-11-28 15:10:11 -0600

    Update detection.py

--- detection.py
+++ detection.py
@@ -23,11 +23,11 @@ while(True):
 
 	for pid in os.listdir('/proc'):
 		if pid.isdigit() and pid not in pids:
-			alertfile.write("Alert!")
-			print("Alert!")
+			alertfile.write("Alert! " + str(pid) + " is attacking the system!")
+			print("Alert! " + str(pid) + " is attacking the system!")
 
 	for line in lines:
 		#look for weirdness
 		if line == "weirdness":
-			print("Alert! Attack in progress!")
-			alertfile.write("Alert! Attack in progress!")
+			print("Alert! " + str(pid) + " is attacking the system!")
+			alertfile.write("Alert! " + str(pid) + " is attacking the system!")
